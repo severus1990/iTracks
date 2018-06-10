b0bf0212ff0bd0e9664026e7772ed5c27e628a14
Rahul Raj Rajan01 <rahul_rajan01@infosys.com>
2018-06-10 11:41:41 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAEputEBnpSQ2ZsvMG_tW_23ZePMTyorUI",
+  authDomain: "sampleservice-c9f34.firebaseapp.com",
+  databaseURL: "https://sampleservice-c9f34.firebaseio.com",
+  projectId: "sampleservice-c9f34",
+  storageBucket: "sampleservice-c9f34.appspot.com",
+  messagingSenderId: "599413645702"
+};
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +24,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  messagesDummy: Array<any> = [ {
+    "alert" : "",
+    "carrierMess" : "Message sent to Driver to initiate Order",
+    "delay" : 0,
+    "message" : "This is a reminder to turn on tracking for the XPO load you’ll pick up in 2 hours.",
+    "time" : "11:00",
+    "toDriver" : true
+  }, {
+    "alert" : "",
+    "carrierMess" : "Driver travelling on time.",
+    "delay" : 0,
+    "message" : "Have you been loaded with the correct weight and quantity per the BOL. Say YES or NO.",
+    "time" : "12:00",
+    "toDriver" : true
+  }, {
+    "alert" : "",
+    "carrierMess" : "Driver delayed. Alert Sent to Driver",
+    "delay" : 30,
+    "message" : "You are scheduled to arrive at 3pm, will you be on time? Say YES or NO.",
+    "time" : "14:00",
+    "toDriver" : true
+  },
+  {
+    "alert" : "",
+    "carrierMess" : "Order Delivered on time by driver.",
+    "delay" : 0,
+    "message" : "Please contact your carrier representative for payment details. Thank you.",
+    "time" : "17:00",
+    "toDriver" : true
+  } 
+]
+  i: any
+  bidding:AngularFireList<any>;
+  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+    this.i=0;
+    this.bidding = this.db.list('/itracks/messages');
+  }
 
-  constructor(public navCtrl: NavController) {
+  sendMe(){
+    console.log(this.messagesDummy[this.i]);
+    this.bidding.update((this.i).toString(),this.messagesDummy[this.i]);
+    this.i++;
 
   }
 
